36a9ad8712756b7fb7fa2c98399dcaff22d935e8
amit777 <amithegde777@gmail.com>
2023-08-25 13:02:08 +0530

    max depth

---
+++ 15-Trees/03-Questions/02-Max Depth/maxDepth.java
@@ -0,0 +1,23 @@
+//https://leetcode.com/problems/maximum-depth-of-binary-tree/submissions/?envType=study-plan-v2&envId=top-interview-150
+package binaryTree.Questions;
+
+public class maxDepth {
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+    public int maxDepth(TreeNode root) {
+        if(root==null) return 0;
+        int left=maxDepth(root.left);
+        int right=maxDepth(root.right);
+        return Math.max(left,right)+1;
+    }
+}
